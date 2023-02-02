6d8777bf45ef19d43046556157a6cb0812885383
IgorFilin <filinigor@yandex.ru>
2023-02-02 22:12:54 +0300

    feat: create grades and icon openweathermap

--- src/ui/components/Weather/WeatherInCityBlock/WeatherInCityBlock.tsx
+++ src/ui/components/Weather/WeatherInCityBlock/WeatherInCityBlock.tsx
@@ -9,8 +9,13 @@ export const WeatherInCityBlock: React.FC<IWeatherInCityBlock> = () => {
 
     const weather = useAppSelector(state => state.weatherReducer.weather)
 
-    const city = weather?.name?.toUpperCase()
-    const country = weather?.sys?.country
+    const city = weather.name?.toUpperCase()
+    const country = weather.sys?.country
+    const description = weather.weather && weather?.weather[0]?.description?.slice(0,1).toUpperCase() + weather?.weather[0]?.description?.slice(1)
+    const grades = Math.round(weather?.main?.temp)
+    const icon = weather.weather && weather?.weather[0].icon
+
+
 
     return (
         <Box>
@@ -25,6 +30,16 @@ export const WeatherInCityBlock: React.FC<IWeatherInCityBlock> = () => {
                     <img src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${country}.svg`} alt="flag"
                          width='50px' height='30px'/>
                     <Typography fontWeight={700} variant="h3" component="h3">{city}</Typography>
+                    <Typography fontWeight={400} variant="h6" component="h6">{description}</Typography>
+                    <Box sx={{
+                        display:'flex',
+                        justifyContent:'center',
+                        gap:'5px',
+                    }}>
+                        <Typography fontWeight={400} variant="h3" component="h3">{grades}</Typography>
+                        <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="weather description"
+                             width='50px' height='50px'/>
+                    </Box>
                 </Box>}
         </Box>
     );
