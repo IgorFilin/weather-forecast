d82cffd9eb2744647efdda41e758147698cbdd13
IgorFilin <filinigor@yandex.ru>
2023-02-03 22:07:17 +0300

    feat: create style scroll

--- src/utils/hooks/useChangeMuiTheme.ts
+++ src/utils/hooks/useChangeMuiTheme.ts
@@ -1,5 +1,6 @@
 import {createTheme, PaletteMode} from "@mui/material";
 import {useMemo, useState} from "react";
+import {grey} from "@mui/material/colors";
 
 
 export function useChangeMuiTheme() {
@@ -10,8 +11,36 @@ export function useChangeMuiTheme() {
     const getDesignTokens = (mode: PaletteMode) => ({
         palette: {
             mode,
-        }
-    })
+        },
+        components: {
+            MuiCssBaseline: {
+                styleOverrides: {
+                    body: {
+                        '&::-webkit-scrollbar, & *::-webkit-scrollbar': {
+                            width: '0.9rem',
+                        },
+                        '&::-webkit-scrollbar-thumb, & *::-webkit-scrollbar-thumb': {
+                            borderRadius: 8,
+                            backgroundColor: '#333333',
+                            minHeight: 24,
+                            border: '3px solid #f5f5f5',
+                        },
+                        '&::-webkit-scrollbar-thumb:focus, & *::-webkit-scrollbar-thumb:focus':
+                            {
+                                backgroundColor: grey[600],
+                            },
+                        '&::-webkit-scrollbar-thumb:active, & *::-webkit-scrollbar-thumb:active':
+                            {
+                                backgroundColor: grey[600],
+                            },
+                        '&::-webkit-scrollbar-thumb:hover, & *::-webkit-scrollbar-thumb:hover':
+                            {
+                                backgroundColor: grey[600],
+                            },
+                    },
+                },
+            },
+        }})
 
 
     const colorMode = useMemo(
