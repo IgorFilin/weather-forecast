0cda995ce4d0664b179ccf65da9de26daf097f59
IgorFilin <filinigor@yandex.ru>
2023-02-04 22:55:59 +0300

    feat: list time

--- src/ui/components/Weather/WeatherHourlyAndDaily/WeatherHourlyAndDaily.tsx
+++ src/ui/components/Weather/WeatherHourlyAndDaily/WeatherHourlyAndDaily.tsx
@@ -1,14 +1,24 @@
 import React, {useEffect} from 'react';
-import {Box} from "@mui/material";
+import {Box, Skeleton, Typography} from "@mui/material";
 import {useAppDispatch} from "../../../../utils/hooks/useAppDispatch";
 import {weatherHourlyAndDailyThunk} from "../../../../bll/thunk/weatherHourlyAndDailyThunk";
 import {useAppSelector} from "../../../../utils/hooks/useAppSelector";
+import {converterUnixAndGetDate} from "../../../../utils/functions/convertedUnixInRealTime";
+import moment from "moment";
 
 export const WeatherHourlyAndDaily = () => {
 
     const weather = useAppSelector(state => state.weatherReducer.weather)
+    const weatherHourlyAndDaily = useAppSelector(state => state.weatherReducer.weatherHourlyAndDaily)
+    const isLoading = useAppSelector(state => state.weatherReducer.isLoading)
 
     const dispatch = useAppDispatch()
+
+
+    const hourly = (weatherHourlyAndDaily?.hourly)
+    // moment.unix.format("HH:00")
+    console.log(hourly)
+
     useEffect(() => {
         if(Object.values(weather).length !== 0){
             dispatch(weatherHourlyAndDailyThunk({lat:weather.coord.lat,lon:weather.coord.lon}))
@@ -17,8 +27,23 @@ export const WeatherHourlyAndDaily = () => {
     }, [weather])
 
     return (
-        <Box>
-
+        <Box sx={{
+            display:'flex',
+            justifyContent:'center',
+            width:'80%',
+            flexWrap:'wrap',
+            gap:'10px',
+        }}>
+            {hourly?.map(hour => {
+                return <Box>
+                    {isLoading ? <Skeleton width='130px' height='33px'/> :
+                        <Typography sx={{
+                            color: 'rgb(133,83,83)',
+                        }} fontWeight={400} variant="h5" component="h6">
+                            {moment.unix(hour.dt).format("HH:00")}
+                        </Typography>}
+                </Box>
+            })}
         </Box>
     );
 };
